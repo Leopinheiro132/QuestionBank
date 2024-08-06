3ead545445fe8c178e220bc4d16d11212f4430fe
Leo Pinheiro <53160521+Leopinheiro132@users.noreply.github.com>
2024-08-06 18:37:34 -0300

    Add files via upload

--- src/pages/testandocoisas.jsx
+++ src/pages/testandocoisas.jsx
@@ -1,35 +1,4 @@
-import Questao from './_components/Question/questao';
 import SideMenu from './_components/Sidemenu/sidemenu';
-
-const questoes = [
-    {
-        conteudo: {
-            banca: 'Banca 1',
-            ano: '2023',
-            materia: 'Matemática',
-            texto: 'Qual é a raiz quadrada de 144?',
-            alternativas: [
-                { label: '10', value: '10' },
-                { label: '12', value: '12' },
-                { label: '14', value: '14' }
-            ]
-        }
-    },
-    {
-        conteudo: {
-            banca: 'Banca 2',
-            ano: '2022',
-            materia: 'História',
-            texto: 'Quem descobriu o Brasil?',
-            alternativas: [
-                { label: 'Pedro Álvares Cabral', value: 'Pedro Álvares Cabral' },
-                { label: 'Cristóvão Colombo', value: 'Cristóvão Colombo' },
-                { label: 'Vasco da Gama', value: 'Vasco da Gama' }
-            ]
-        }
-    }
-    // Adicione mais questões conforme necessário
-];
 
 export default function Questoes() {
     return (
@@ -38,13 +7,4 @@ export default function Questoes() {
         </div>
     );
 }
-/**
- *             {questoes.map((questao, index) => (
-                <Questao 
-                    key={index}
-                    conteudo={questao.conteudo}
-                    indice={index}
-                />
-            ))}
- * 
- */
+
