74c450df0ee242b7e830979cd469c83f6b41f917
Leo Pinheiro <53160521+Leopinheiro132@users.noreply.github.com>
2024-08-12 00:15:36 -0300

    Add files via upload

---
+++ pages/_components/Header/CommonHeader.jsx
@@ -0,0 +1,15 @@
+import styles from '../../../styles/Head.module.css'
+import { useRouter } from 'next/router';
+
+const CommonHeader = ({ children }) => {
+  const router = useRouter();
+
+  return (
+    <header className={styles.head}>
+      <a onClick={() => router.push('/')}><img className={styles.headImg} src='/img/Head/QB-logo.svg' alt="Logo" /></a>
+      {children}
+    </header>
+  );
+};
+
+export default CommonHeader;
---
+++ pages/api/dashperguntas.js
@@ -0,0 +1,69 @@
+import {connectDB, disconnectDB } from '../../../_ultils/mongoDbconnect';
+import Pergunta from '../../../models/model-Pergunta';
+
+connectDB();
+
+export default async function handler(req, res) {
+  try {
+    const perguntasPorMateria = await Pergunta.aggregate([
+      {
+        $group: {
+          _id: "$materia",
+          quantidade: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          materia: "$_id",
+          quantidade: 1,
+          _id: 0
+        }
+      }
+    ]);
+
+    const perguntasPorAno = await Pergunta.aggregate([
+      {
+        $group: {
+          _id: "$ano",
+          quantidade: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          ano: "$_id",
+          quantidade: 1,
+          _id: 0
+        }
+      }
+    ]);
+
+    // Dados agrupados por banca
+    const perguntasPorBanca = await Pergunta.aggregate([
+      {
+        $group: {
+          _id: "$banca",
+          quantidade: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          banca: "$_id",
+          quantidade: 1,
+          _id: 0
+        }
+      }
+    ]);
+
+    res.status(200).json({ 
+      success: true, 
+      data: {
+        porMateria: perguntasPorMateria,
+        porAno: perguntasPorAno,
+        porBanca: perguntasPorBanca
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+}
